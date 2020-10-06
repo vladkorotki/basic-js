c500285bbc10c3a53deba6c82dd23caf07a27f1c
Короткий Владислав Владимирович <vladislav.korotky@becloud.by>
2020-10-06 05:51:03 +0300

    end carbon task

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,9 +1,21 @@
 const CustomError = require("../extensions/custom-error");
 
-const MODERN_ACTIVITY= 15; 
-const HALF_LIFE_PERIOD= 5730;
+const MODERN_ACTIVITY = 15;
+const HALF_LIFE_PERIOD = 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function dateSample(sampleActivity) {
+
+  if (typeof sampleActivity === 'string' && parseFloat(sampleActivity) !== NaN && parseFloat(sampleActivity) > 0 && parseFloat(sampleActivity) <= 15 && sampleActivity !== 'undefined' && sampleActivity !== undefined) {
+    let A = Math.log2(MODERN_ACTIVITY / parseFloat(sampleActivity));
+    let K = 0.693 / HALF_LIFE_PERIOD;
+    let number = Math.ceil(A / K);
+
+    return number;
+  } else {
+    return false;
+    throw new CustomError('Not implemented');
+  }
+
 };
+
+
