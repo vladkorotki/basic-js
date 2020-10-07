da5ac945aeea29a089bea1f62a43500ea6469794
Vlad Korotki <vladkorotki92@gmail.com>
2020-10-07 21:24:23 +0300

    end Dream Team task

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,28 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
+module.exports = function createDreamTeam(members) {
+  //throw new CustomError('Not implemented');
   // remove line with error and write your code here
+  let teamName = '';
+  if (Array.isArray(members)) {
+    for (let i = 0; i < members.length; i++) {
+      if (typeof members[i] === 'string') {
+        for (let char of members[i]) {
+          if (char !== ' ') {
+            teamName += char.toUpperCase();
+            break;
+          } else {
+            continue;
+          }
+        }
+      } else {
+        continue;
+      }
+    }
+    teamName = teamName.split('').sort();
+    teamName = teamName.join('');
+    return teamName;
+  } else {
+    return false;
+  }
 };
